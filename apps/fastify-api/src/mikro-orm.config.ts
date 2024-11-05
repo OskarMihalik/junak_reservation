47919deeeeca308a04cdd72ce7807fa0726fc7b8
Oskar Mihalik <mihalikoskar@gmail.com>
2024-11-05 19:37:43 +0100

    feat: setup postgres

--- apps/fastify-api/src/mikro-orm.config.ts
+++ apps/fastify-api/src/mikro-orm.config.ts
@@ -1,11 +1,16 @@
-import { type Options, SqliteDriver } from '@mikro-orm/sqlite'
+// import { type Options, SqliteDriver } from '@mikro-orm/sqlite'
 import { TsMorphMetadataProvider } from '@mikro-orm/reflection'
 import { Migrator } from '@mikro-orm/migrations'
+import { type Options, PostgreSqlDriver } from '@mikro-orm/postgresql'
 
 const config: Options = {
   // for simplicity, we use the SQLite database, as it's available pretty much everywhere
-  driver: SqliteDriver,
-  dbName: 'sqlite.db',
+  driver: PostgreSqlDriver,
+  dbName: 'postgres',
+  user: 'admin',
+  host: 'localhost',
+  port: 5435,
+  password: 'admin',
   // folder-based discovery setup, using common filename suffix
   entities: ['dist/**/*.entity.js'],
   entitiesTs: ['src/**/*.entity.ts'],
