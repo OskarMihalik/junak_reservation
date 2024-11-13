1e7b3a48b7b78cd1820513992f2708d768efafb6
Patrik Antal <patrik.antal3@gmail.com>
2024-11-14 00:34:46 +0100

    Add other entities

---
+++ apps/fastify-api/src/modules/schedule/schedule.entity.ts
@@ -0,0 +1,27 @@
+import { Entity, PrimaryKey, Property, Enum, OneToMany, Collection } from '@mikro-orm/core';
+import { DaySchedule } from "../daySchedule/daySchedule.entity";
+
+export enum DayEnum {
+  MON = 'MON',
+  TUE = 'TUE',
+  WED = 'WED',
+  THU = 'THU',
+  FRI = 'FRI',
+  SAT = 'SAT',
+  SUN = 'SUN',
+}
+
+@Entity()
+export class Schedule {
+  @PrimaryKey({ autoincrement: true })
+  id!: number;
+
+  @Enum(() => DayEnum)
+  day!: DayEnum;
+
+  @Property({ onCreate: () => new Date() })
+  start: Date = new Date();
+
+  @OneToMany(() => DaySchedule, daySchedule => daySchedule.schedule)
+  daySchedules = new Collection<DaySchedule>(this);
+}
