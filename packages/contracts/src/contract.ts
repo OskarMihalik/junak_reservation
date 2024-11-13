27f158f8cc84db63772581c8c90f964bba753c8f
Oskar Mihalik <mihalikoskar@gmail.com>
2024-11-13 22:51:36 +0100

    feat: remove alias

--- packages/contracts/src/contract.ts
+++ packages/contracts/src/contract.ts
@@ -2,10 +2,10 @@ import { initContract, type RouterOptions } from '@ts-rest/core'
 import { apiHelloContract } from './features/hello.contract'
 import { API_PATH_PREFIX } from './constants'
 import { apiUserContract } from './features/user.contract'
-import { apiAdminScheduleContract } from "@/features/adminSchedule.contract";
-import { apiScheduleContract } from "@/features/schedule.contract";
-import { apiAdminSubscriptionContract } from "@/features/adminSubscription.contract";
-import { apiSubscriptionContract } from "@/features/subscription.contract";
+import { apiAdminScheduleContract } from './features/adminSchedule.contract'
+import { apiAdminSubscriptionContract } from './features/adminSubscription.contract'
+import { apiScheduleContract } from './features/schedule.contract'
+import { apiSubscriptionContract } from './features/subscription.contract'
 
 const c = initContract()
 
