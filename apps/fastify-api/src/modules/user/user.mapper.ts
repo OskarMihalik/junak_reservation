28896a3aa17a74a291d8bae4257c8a19c2d4e14e
Patrik Antal <patrik.antal3@gmail.com>
2024-11-14 02:46:07 +0100

    Update mapper for User

--- apps/fastify-api/src/modules/user/user.mapper.ts
+++ apps/fastify-api/src/modules/user/user.mapper.ts
@@ -2,6 +2,7 @@ import { User } from './user.entity';
 
 export function mapUserToDto(user: User) {
   return {
+    id: user.id,
     name: user.name,
     surname: user.surname,
     aisId: user.aisId,
