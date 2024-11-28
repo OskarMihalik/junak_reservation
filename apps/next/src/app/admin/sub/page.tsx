ee3a019589c2e715cfdd1cfdd7cb3b8c2a65c08b
vrabelmarian <mrn.vrabel@gmail.com>
2024-11-28 19:21:25 +0100

    feat: admin revoke subscriptions

--- apps/next/src/app/admin/sub/page.tsx
+++ apps/next/src/app/admin/sub/page.tsx
@@ -9,9 +9,9 @@ const SubscriptionsPage: React.FC = () => {
 
   const { data, refetch } = client.adminSubscription.getSubscriptionsAsync.useQuery(['getSubscriptionsAsync'])
 
-  const { mutate } = client.adminSubscription.approveSubscriptionAsync.useMutation({
+  const { mutate: approveMutate } = client.adminSubscription.approveSubscriptionAsync.useMutation({
     onSuccess: function(data) {
-      if (data.status == 200) {
+      if (data.status === 200) {
         toast({ description: 'Subscription approved' })
         refetch()
       }
@@ -20,22 +20,41 @@ const SubscriptionsPage: React.FC = () => {
       toast({ description: 'Subscription failed to approve' })
     },
   })
+
+  const { mutate: revokeMutate } = client.adminSubscription.revokeSubscriptionAsync.useMutation({
+    onSuccess: function(data) {
+      if (data.status === 200) {
+        toast({ description: 'Subscription revoked' })
+        refetch()
+      }
+    },
+    onError: () => {
+      toast({ description: 'Subscription failed to revoke' })
+    },
+  })
+
   const handleApprove = (id: number) => {
     console.log(`Approve ID: ${id}`)
-    mutate({ params: { id: id.toString() } })
+    approveMutate({ params: { id: id.toString() } })
   }
 
   const handleRevoke = (id: number) => {
     console.log(`Revoke ID: ${id}`)
+    revokeMutate({ params: { id: id.toString() } })
   }
+
+  const sortedData = data?.body?.sort((a, b) => {
+    return a.id - b.id
+  })
 
   return (
     <div className="container mx-auto p-4 text-gray-100 min-h-screen">
       <h1 className="text-3xl font-bold mb-6 text-white">Subscriptions</h1>
-      {data && data.body?.length ? (
+      {sortedData && sortedData.length ? (
         <table className="min-w-full border-collapse border border-gray-700 rounded-lg shadow-sm bg-gray-800">
           <thead className="bg-gray-700">
           <tr>
+            <th className="border border-gray-700 px-4 py-3 text-left text-gray-300">ID</th>
             <th className="border border-gray-700 px-4 py-3 text-left text-gray-300">Full Name</th>
             <th className="border border-gray-700 px-4 py-3 text-left text-gray-300">AIS ID</th>
             <th className="border border-gray-700 px-4 py-3 text-left text-gray-300">Variable Symbol</th>
@@ -45,14 +64,15 @@ const SubscriptionsPage: React.FC = () => {
           </tr>
           </thead>
           <tbody>
-          {data.body.map((subscription, index) => (
-            <tr key={subscription.id || index} className="even:bg-gray-850 hover:bg-gray-600">
+          {sortedData.map((subscription) => (
+            <tr key={subscription.id} className="even:bg-gray-850 hover:bg-gray-600">
+              <td className="border border-gray-700 px-4 py-3">{subscription.id}</td>
               <td className="border border-gray-700 px-4 py-3">
-                {subscription.user ? `${subscription.user.name} ${subscription.user.surname}` : 'N/A'}
+                {subscription.user
+                  ? `${subscription.user.name} ${subscription.user.surname}`
+                  : 'N/A'}
               </td>
-              <td className="border border-gray-700 px-4 py-3">
-                {subscription.user?.aisId ?? 'N/A'}
-              </td>
+              <td className="border border-gray-700 px-4 py-3">{subscription.user?.aisId ?? 'N/A'}</td>
               <td className="border border-gray-700 px-4 py-3">{subscription.variableSymbol}</td>
               <td className="border border-gray-700 px-4 py-3">{subscription.subscriptionPeriod}</td>
               <td className="border border-gray-700 px-4 py-3 capitalize">{subscription.status}</td>
