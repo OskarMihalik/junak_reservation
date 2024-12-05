f10a58f946314b4e338e06030efa97a046dc15c1
Oskar Mihalik <mihalikoskar@gmail.com>
2024-12-05 19:10:59 +0100

    feat: docker env host

--- apps/fastify-api/src/main.ts
+++ apps/fastify-api/src/main.ts
@@ -2,7 +2,7 @@ import Fastify, { type FastifyInstance } from 'fastify'
 import { app } from './app/app.js'
 import { SubscriptionValidator } from './app/utils/subscriptionValidator.js'
 
-export const HOST = 'localhost'
+export const HOST = process.env.HOST ?? 'localhost'
 export const PORT = process.env.PORT ? Number(process.env.PORT) : 3939
 
 export const fastifyInstance: FastifyInstance = Fastify({ logger: true, disableRequestLogging: false })
@@ -13,7 +13,7 @@ const start = async (): Promise<void> => {
 
     await SubscriptionValidator.checkAndExpireSubscriptionsAsync()
 
-    await fastifyInstance.listen({ host: '0.0.0.0', port: PORT })
+    await fastifyInstance.listen({ host: HOST, port: PORT })
   } catch (error: unknown) {
     fastifyInstance.log.error(error)
     process.exit(1)
